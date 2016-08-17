2782f67f2c1dfbdb7dcc3aacf876c685f0e4e18e
dmonyo <devel.enterprise@gmail.com>
2016-08-17 00:14:35 -0400

    changes in README

--- frontend/application/views/header.php
+++ frontend/application/views/header.php
@@ -2,8 +2,7 @@
 <html lang="es">
 
 <head>
-    <title>Cineworld - Back Office</title>
-    <link rel="shortcut icon" href="http://anexsoft.com/assets/frontend/images/favicon.ico" />
+    <title>Person Registration</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <meta charset="utf-8" />
 
